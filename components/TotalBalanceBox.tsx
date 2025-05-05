e88e63c70c0a270582e0334a57803fb11035d314
Reece <reecewatson100@gmail.com>
2025-05-05 18:30:49 +1000

    implement initial file and folder structure

---
+++ components/TotalBalanceBox.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const TotalBalanceBox = ({
+  accounts = [],
+  totalBanks,
+  totalCurrentBalance,
+}: TotlaBalanceBoxProps) => {
+  return <section className='total-balance'>
+    <div className='total-balance-chart'></div>
+  </section>;
+};
+
+export default TotalBalanceBox;
